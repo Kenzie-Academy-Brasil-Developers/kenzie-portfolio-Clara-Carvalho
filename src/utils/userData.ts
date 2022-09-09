9d43dee466a98636b49b9bdcf19de4578e4000e2
Clara Cavalcantee <claracarvc@gmail.com>
2022-09-09 00:35:21 -0300

    portifolio

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,6 +1,6 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "samuelleao",
+  nameUser: "Felipe Cavalcante",
+  githubUser: "Felipecmc",
+  linkedinUser: "felipe-cavalcante-42971b203",
   whatsappNumber: "+5541111111111",
 };
